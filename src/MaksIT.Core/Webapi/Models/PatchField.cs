863885c9a15e6414f2fc4266ea79ae47e249abcd
Maksym Sadovnychyy <maksym.sadovnychyy@gmail.com>
2024-10-27 11:37:07 +0100

    (feature): patch field deconstruct

--- src/MaksIT.Core/Webapi/Models/PatchField.cs
+++ src/MaksIT.Core/Webapi/Models/PatchField.cs
@@ -4,4 +4,9 @@ namespace MaksIT.Core.Webapi.Models;
 public class PatchField<T> {
   public PatchOperation Operation { get; set; }
   public T? Value { get; set; }
+
+  public void Deconstruct(out PatchOperation operation, out T? value) {
+    operation = Operation;
+    value = Value;
+  }
 }
