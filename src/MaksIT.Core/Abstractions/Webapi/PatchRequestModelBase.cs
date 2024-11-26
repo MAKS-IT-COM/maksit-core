d83972278201b0809889282d9cd9da918c04f8ed
Maksym Sadovnychyy <maksym.sadovnychyy@gmail.com>
2024-11-26 22:15:25 +0100

    (feature): patch request model

---
+++ src/MaksIT.Core/Abstractions/Webapi/PatchRequestModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using MaksIT.Core.Webapi.Models;
+
+
+namespace MaksIT.Core.Abstractions.Webapi;
+
+public abstract class PatchRequestModelBase : RequestModelBase {
+  public bool HasNonNullPatchField => GetType()
+    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+    .Where(prop => prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(PatchField<>))
+    .Any(prop => prop.GetValue(this) != null);
+}
--- src/MaksIT.Core/Webapi/Models/PatchOperation.cs
+++ src/MaksIT.Core/Webapi/Models/PatchOperation.cs
@@ -1,8 +1,24 @@
 ﻿namespace MaksIT.Core.Webapi.Models;
 
 public enum PatchOperation {
+
+  /// <summary>
+  /// When you need to replace some field, or relpace item in collection
+  /// </summary>
   Replace,
+
+  /// <summary>
+  /// When you need to set some field, or add item to collection
+  /// </summary>
   Add,
+
+  /// <summary>
+  /// When you need to set some field to null, or remove item from collection
+  /// </summary>
   Remove,
-  Clear // for collections
+
+  /// <summary>
+  /// When you need to clear collection
+  /// </summary>
+  Clear
 }
