1e51993d1340039830184f056cebf67d75320847
Maksym Sadovnychyy <maksym.sadovnychyy@gmail.com>
2024-11-28 17:15:52 +0100

    (feature): patch model base, validatable object

--- src/MaksIT.Core/Abstractions/Webapi/PatchRequestModelBase.cs
+++ src/MaksIT.Core/Abstractions/Webapi/PatchRequestModelBase.cs
@@ -1,12 +1,19 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using MaksIT.Core.Webapi.Models;
 
 
 namespace MaksIT.Core.Abstractions.Webapi;
 
-public abstract class PatchRequestModelBase : RequestModelBase {
-  public bool HasNonNullPatchField => GetType()
+public abstract class PatchRequestModelBase : RequestModelBase, IValidatableObject {
+  private bool HasNonNullPatchField => GetType()
     .GetProperties(BindingFlags.Public | BindingFlags.Instance)
     .Where(prop => prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(PatchField<>))
     .Any(prop => prop.GetValue(this) != null);
+
+  public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
+    if (!HasNonNullPatchField) {
+      yield return new ValidationResult("At least one patch field must be provided", new string[] { "PatchField" });
+    }
+  }
 }
