d7b0145f084bd8d58345d2c0a3f6c772baf44b7d
Maksym Sadovnychyy <maksym.sadovnychyy@gmail.com>
2024-12-15 00:58:45 +0100

    (feature): TryGetOperation improvements

--- src/MaksIT.Core/Abstractions/Webapi/PatchRequestModelBase.cs
+++ src/MaksIT.Core/Abstractions/Webapi/PatchRequestModelBase.cs
@@ -2,21 +2,34 @@
 using System.ComponentModel.DataAnnotations;
 
 using MaksIT.Core.Webapi.Models;
+using System.Diagnostics.CodeAnalysis;
 
 
 namespace MaksIT.Core.Abstractions.Webapi;
 
 public abstract class PatchRequestModelBase : RequestModelBase {
 
-  public Dictionary<string, PatchOperation> Operations = new Dictionary<string, PatchOperation>();
+  public Dictionary<string, PatchOperation>? Operations { get; set; }
 
   private bool HasNonNullPatchField => GetType()
     .GetProperties(BindingFlags.Public | BindingFlags.Instance)
     .Where(prop => prop.Name != nameof(Operations))
     .Any(prop => prop.GetValue(this) != null);
 
-  public bool TryGetOperation(string propertyName, out PatchOperation operation) {
-    return Operations.TryGetValue(propertyName, out operation);
+  public bool TryGetOperation(string propertyName, [NotNullWhen(true)] out PatchOperation? operation) {
+    if (Operations == null) {
+      operation = null;
+      return false;
+    }
+
+    if (Operations.TryGetValue(propertyName, out var tempOperation)) {
+      operation = tempOperation;
+      return true;
+    }
+    else {
+      operation = null;
+      return false;
+    }
   }
 
   public override IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
@@ -24,9 +37,11 @@ public abstract class PatchRequestModelBase : RequestModelBase {
       yield return new ValidationResult("At least one patch field must be provided", new string[] { "PatchField" });
     }
 
-    foreach (var operation in Operations) {
-      if (!Enum.IsDefined(typeof(PatchOperation), operation.Value)) {
-        yield return new ValidationResult($"Invalid patch operation '{operation.Value}' for property '{operation.Key}'", new string[] { operation.Key });
+    if (Operations != null) {
+      foreach (var operation in Operations) {
+        if (!Enum.IsDefined(typeof(PatchOperation), operation.Value)) {
+          yield return new ValidationResult($"Invalid patch operation '{operation.Value}' for property '{operation.Key}'", new string[] { operation.Key });
+        }
       }
     }
   }
