bbe40000956d59218023dbcaa1ee86e0e2598111
Maksym Sadovnychyy <maksym.sadovnychyy@gmail.com>
2025-05-25 21:07:38 +0200

    (feature): webapi middlewares

---
+++ src/MaksIT.Core/Webapi/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+
+namespace MaksIT.Core.Webapi.Middlewares;
+
+public class ErrorHandlingMiddleware {
+  private readonly RequestDelegate _next;
+  private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+  public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger) {
+    _next = next;
+    _logger = logger;
+  }
+
+  public async Task Invoke(HttpContext context) {
+    try {
+      await _next(context); // proceed to next middleware
+    }
+    catch (Exception ex) {
+      _logger.LogError(ex, "Unhandled exception");
+
+      context.Response.StatusCode = 500;
+      context.Response.ContentType = "application/json";
+
+      var errorResponse = new {
+        error = "An unexpected error occurred.",
+        details = ex.Message // or omit in production
+      };
+
+      var json = JsonSerializer.Serialize(errorResponse);
+      await context.Response.WriteAsync(json);
+    }
+  }
+}
---
+++ src/MaksIT.Core/Webapi/Middlewares/TraceIdLoggingScopeMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MaksIT.Core.Webapi.Middlewares;
+
+public class TraceIdLoggingScopeMiddleware {
+  private readonly RequestDelegate _next;
+  private readonly ILogger<TraceIdLoggingScopeMiddleware> _logger;
+
+  public TraceIdLoggingScopeMiddleware(RequestDelegate next, ILogger<TraceIdLoggingScopeMiddleware> logger) {
+    _next = next;
+    _logger = logger;
+  }
+
+  public async Task InvokeAsync(HttpContext context) {
+    var traceId = Activity.Current?.TraceId.ToString() ?? context.TraceIdentifier;
+
+    using (_logger.BeginScope(new Dictionary<string, object> { ["TraceId"] = traceId })) {
+      await _next(context);
+    }
+  }
+}
