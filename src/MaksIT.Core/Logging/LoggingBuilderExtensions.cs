c09e268e4deccdd2c6eee62520e433c90b9d1f42
Maksym Sadovnychyy <maksym.sadovnychyy@gmail.com>
2025-05-25 21:01:24 +0200

    (feature): logger builder console extensions

--- src/MaksIT.Core/Logging/LoggingBuilderExtensions.cs
+++ src/MaksIT.Core/Logging/LoggingBuilderExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.Extensions.Hosting;
 
 namespace MaksIT.Core.Logging;
 
@@ -8,5 +8,22 @@ public static class LoggingBuilderExtensions {
   public static ILoggingBuilder AddFile(this ILoggingBuilder builder, string filePath) {
     builder.Services.AddSingleton<ILoggerProvider>(new FileLoggerProvider(filePath));
     return builder;
+  }
+  public static ILoggingBuilder AddConsole(this ILoggingBuilder logging, IHostEnvironment env) {
+    logging.ClearProviders();
+    if (env.IsDevelopment()) {
+      logging.AddSimpleConsole(options => {
+        options.IncludeScopes = true;
+        options.SingleLine = false;
+        options.TimestampFormat = "hh:mm:ss ";
+      });
+    }
+    else {
+      logging.AddJsonConsole(options => {
+        options.IncludeScopes = true;
+        options.TimestampFormat = "yyyy-MM-ddTHH:mm:ss.fffZ";
+      });
+    }
+    return logging;
   }
 }
