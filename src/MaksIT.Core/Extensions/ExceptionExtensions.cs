ab7fc584060b8982d2b8145c4010381563ad371f
Maksym Sadovnychyy <maksym.sadovnychyy@gmail.com>
2025-07-25 21:36:57 +0200

    (feature): extract messages from exception

---
+++ src/MaksIT.Core/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace MaksIT.Core.Extensions;
+
+public static class ExceptionExtensions {
+  /// <summary>
+  /// Extracts all messages from an exception and its inner exceptions.
+  /// </summary>
+  /// <param name="exception">The exception to extract messages from.</param>
+  /// <returns>A list of exception messages.</returns>
+  public static List<string> ExtractMessages(this Exception exception) {
+    var messages = new List<string>();
+    var current = exception;
+
+    while (current != null) {
+      messages.Add(current.Message);
+      current = current.InnerException;
+    }
+
+    return messages;
+  }
+}
